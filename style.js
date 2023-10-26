7cc07ee24ee95b345808ef5087be01652731bfaa
TryTon55 <110990002+TryTon55@users.noreply.github.com>
2023-10-25 22:28:06 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,30 @@
+document.querySelector(".scroll-top").addEventListener("click", function() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  });
+  window.scrollTo({
+    top: window.pageYOffset,
+    left: 0,
+  });
+  document.querySelector(".scroll-top").addEventListener("click", function() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  });
+  window.scrollTo({
+    top: window.pageYOffset,
+    left: 0,
+  });
+<button class="scroll-top">
+  <i class="fas fa-arrow-up"></i>
+</button>
+document.querySelector(".scroll-top").addEventListener("click", function() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  });
+          
